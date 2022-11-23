fd65b91013360fb2e38711d9d763422457ee45bf
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2022-11-23 15:04:57 -0300

    atualização produto show

--- Alpha2go/resources/views/produto/show.blade.php
+++ Alpha2go/resources/views/produto/show.blade.php
@@ -1,3 +1,32 @@
-{{$produto->PRODUTO_NOME}}
-{{$produto->Categoria->CATEGORIA_NOME}}
-{{$produto->Imagem->IMAGEM_NOME}}
+//{{$produto->PRODUTO_NOME}}
+//{{$produto->Categoria->CATEGORIA_NOME}}
+//{{$produto->Imagem->IMAGEM_NOME}}
+
+@extends('layouts.store')
+
+@section('content')
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a class="lt-cinza" href="{{url('/')}}">Loja do Senac</a></li>
+        <li class="breadcrumb-item"><a class="lt-cinza" href="{{ route('categoria.show', $produto->categoria->categoria_id) }}">{{ $produto->categoria->categoria_nome }}</a></li>
+        <li class="breadcrumb-item active" aria-current="page"><a class="lt-cinza" href="">{{ $product->name }}</a></li>
+    </ol>
+</nav>
+<div class="row">
+    <div class="col-6 text-center">
+        <img src="{{ asset($produto->imagem) }}" style="width: 250px;">
+    </div>
+    <div class="col-6 text-center">
+        <h2 class="lt-branca"> {{ $produto->PRODUTO_NOME }} </h2>
+        <p class="lt-branca"> {{ $categoria->CATEGORIA_NOME }} </p>
+        <span class="h4 lt-branca"> R$ {{ $produto->Imagem->IMAGEM_NOME }} </span>
+        <div>
+            <a href="{{ route('carrinho.add', $produto->produto_id) }}" class="btn btn-primary"> Adicionar ao carrinho </a>
+            <div class="d-block my-1">
+                <span class="d-block my-1">
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
