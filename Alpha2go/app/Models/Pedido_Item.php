c3b544431b8d5ddd9cb36ff479702641cf4e91ad
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2022-11-23 14:30:47 -0300

    Create Pedido_Item.php

---
+++ Alpha2go/app/Models/Pedido_Item.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pedido_Item extends Model
+{
+  use HasFactory;
+  
+  protected $fillable = [
+  'PRODUTO_ID',
+  'PEDIDO_ID',
+  'ITEM_QTD',
+  'ITEM_PRECO'
+  ];
+
+    protected $table="PEDIDO_ITEM";
+
+    protected $primaryKey = "PEDIDO_ID";
+
+    public $timestamps = false;
+
+    public $foreignKey = "PEDIDO_ID, PRODUTO_ID";
+
+    public function Produto() {
+    return $this->belongsTo(Produto::class, 'PRODUTO_ID');
+}
+  public function Pedido() {
+    return $this->belongsTo(Pedido::class, 'PEDIDO_ID');
+    
+  }
+
+  
+}
