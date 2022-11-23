0227c39603b9a402673b901ceec0cd0cd003be4d
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2022-11-23 14:31:46 -0300

    Create Pedido_Status.php

---
+++ Alpha2go/app/Models/Pedido_Status.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pedido_Status extends Model
+{
+    use HasFactory;
+   
+  protected $fillable = [
+  'STATUS_ID',
+  'STATUS_DESC'
+  ];
+
+    protected $table="PEDIDO_STATUS";
+
+    protected $primaryKey = "STATUS_ID";
+
+    public $timestamps = false;
+
+   
+    public function Pedido() {
+    return $this->hasMany(Pedido::class, 'PEDIDO_ID');
+}
+
+}
