3af663be087eb5ff87f0a6c2b857d5f134e6812c
Milena Galvão <42485813+MiihGalvao@users.noreply.github.com>
2022-11-23 14:00:26 -0300

    atualização carrinho

--- Alpha2go/app/Http/Controllers/CarrinhoController.php
+++ Alpha2go/app/Http/Controllers/CarrinhoController.php
@@ -10,14 +10,42 @@ class CarrinhoController extends Controller
 
 {
     public function add ( Produto $Produto) {
-        dd($produto);
+     $item = Carrinho::where([['PRODUTO_ID','=',$PRODUTO->ID],
+                  ['USUARIO_ID','=',usuario()->id]  
+                  ])->first();
+    if ($item) {
+      $item->update([
+                    'ITEM_QTD' => $item->ITEM_QTD + 1
+      ]);
+      dd('Atualizou a quantidade');
+    }
+      
+        Carrinho::create ([
+                      'USUARIO_ID' => usuario()->id,
+                      'PRODUTO_ID' => $produto->id,
+                      'ITEM_QTD' => 1
+        ]);
+      dd('Adicionou no carrinho');
     }
 
 
-   // public function index(){
-     //   $carrinho_item = Carrinho::where('USUARIO_ID', Auth()->user()->id)->get();
-      //  return view('carrinho.index')->with('carrinho_item', $item_qtd);
-   // }
+   public function remove ( Produto $Produto) {
+     $item = Carrinho::where([['PRODUTO_ID','=',$PRODUTO->ID],
+                  ['USUARIO_ID','=',usuario()->id]  
+                  ])->first();
+    if ($item) {
+      $item->update([
+                    'ITEM_QTD' => $item->ITEM_QTD - 1
+      ]);
+      dd('Atualizou a quantidade para menos');
+    }
+
+     $item->delete();
+      dd('Removeu no carrinho');
+    }
+  
+public function show {
+  $carrinho = Carrinho::where(['USUARIO_ID', '=', usuario()->id])->get();
+  return view ('carrinho,show')->with('carrinho', $carrinho);
 }
-
-
+}
