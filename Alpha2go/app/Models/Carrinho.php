cddd4d2ba54120a726db2a900fc51b3fe1cde12a
MilenaGalvão <milenasg91@gmail.com>
2022-11-23 23:12:18 -0200

    Atualização carrinho, usuario e rotas

--- Alpha2go/app/Models/Carrinho.php
+++ Alpha2go/app/Models/Carrinho.php
@@ -12,8 +12,17 @@ class Carrinho extends Model
 
     protected $table="CARRINHO_ITEM";
 
+    public $timestamps = false;
+
     public function Produto(){
 
-        return $this->belongsTo(Produto::class);
+        return $this->belongsTo(Produto::class, 'PRODUTO_ID', 'PRODUTO_ID');
+    }
+    protected function setKeysForSaveQuery($query) //seleciona as foreign keys
+    {
+        $query->where('USUARIO_ID', '=', $this->getAttribute('USUARIO_ID'))
+              ->where('PRODUTO_ID', '=', $this->getAttribute('PRODUTO_ID'));
+
+        return $query;
     }
 }
